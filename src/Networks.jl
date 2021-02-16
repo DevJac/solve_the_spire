7c309fe345cb01b320094bb1028f1fb26bca7c10
DevJac <dbuttonz@gmail.com>
2021-02-15 17:29:33 -0700

    src/Networks.jl

---
+++ src/Networks.jl
@@ -0,0 +1,58 @@
+module Networks
+using Flux
+using Statistics
+export PolicyNetwork, QNetwork, advantage
+
+#####################
+# Utility Functions #
+#####################
+
+function advantage(network, s)
+    q = network(s)
+    q .- mean(q, dims=1)
+end
+
+##################
+# Policy Network #
+##################
+
+struct PolicyNetwork{N}
+    network :: N
+end
+
+Flux.@functor PolicyNetwork
+
+function PolicyNetwork(in, out, hidden, activation=mish, initW=Flux.kaiming_uniform)
+    layers = Any[Dense(in, hidden[1], activation, initW=initW)]
+    for i in 1:length(hidden)-1
+        push!(layers, Dense(hidden[i], hidden[i+1], activation, initW=initW))
+    end
+    push!(layers, Dense(hidden[end], out, identity))
+    push!(layers, softmax)
+    PolicyNetwork(Chain(layers...))
+end
+
+(p::PolicyNetwork)(s) = p.network(s)
+
+#############
+# Q Network #
+#############
+
+struct QNetwork{N}
+    network :: N
+end
+
+Flux.@functor QNetwork
+
+function QNetwork(in, out, hidden, activation=mish, initW=Flux.kaiming_uniform)
+    layers = Any[Dense(in, hidden[1], activation, initW=initW)]
+    for i in 1:length(hidden)-1
+        push!(layers, Dense(hidden[i], hidden[i+1], activation, initW=initW))
+    end
+    push!(layers, Dense(hidden[end], out, identity))
+    QNetwork(Chain(layers...))
+end
+
+(q::QNetwork)(s) = q.network(s)
+
+end # module
---
+++ test/Networks.jl
@@ -0,0 +1,28 @@
+using Flux
+using Test
+
+using Networks
+
+@testset "advantage" begin
+    p = PolicyNetwork(4, 3, [8])
+    result = advantage(p, rand(4, 10))
+    @test size(result) == (3, 10)
+    @test isapprox(sum(result, dims=1), zeros(1, 10), atol=1e-5)
+end
+
+@testset "PolicyNetwork" begin
+    p = PolicyNetwork(4, 3, [8])
+    @test params(p) .|> length == [32, 8, 24, 3]
+    result = p(rand(4, 10))
+    @test size(result) == (3, 10)
+    @test all(result) do n
+        0 <= n && n <= 1
+    end
+    @test isapprox(sum(result, dims=1), ones(1, 10))
+end
+
+@testset "QNetwork" begin
+    q = QNetwork(4, 3, [8])
+    @test params(q) .|> length == [32, 8, 24, 3]
+    @test size(q(rand(4, 10))) == (3, 10)
+end
