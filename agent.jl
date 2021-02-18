2637c22b30759f0b9658a8ef8f1e40c17804bb56
DevJac <dbuttonz@gmail.com>
2021-02-17 18:56:24 -0700

    agent.jl plays randomly on its own

---
+++ agent.jl
@@ -0,0 +1,135 @@
+using JSON
+using Sockets
+using StatsBase
+
+const SOCKET_FILE = "/home/devjac/Code/julia/solve_the_spire/relay_socket"
+const STATE_LOG_FILE = "/home/devjac/Code/julia/solve_the_spire/state.log"
+
+function hide_map(state)
+    result = deepcopy(state)
+    if "map" in keys(result)
+        result["map"] = "..."
+    end
+    if "game_state" in keys(result)
+        if "map" in keys(result["game_state"])
+            result["game_state"]["map"] = "..."
+        end
+    end
+    result
+end
+
+function run()
+    socket = connect(SOCKET_FILE)
+    open(STATE_LOG_FILE, "a") do state_log_file
+        while true
+            sts_state = JSON.parse(readline(socket))
+            JSON.print(state_log_file, sts_state)
+            write(state_log_file, "\n")
+            JSON.print(stdout, hide_map(sts_state), 12)
+            c = command(sts_state)
+            if isnothing(c)
+                JSON.print(stdout, hide_map(sts_state), 12)
+                print("Command: ")
+                cli_input = ""
+                while length(strip(cli_input)) == 0
+                    cli_input = readline(stdin)
+                end
+                write(socket, cli_input * "\n")
+            else
+                write(socket, c * "\n")
+            end
+        end
+    end
+end
+
+shop_floors = []
+
+function command(state)
+    if "in_game" in keys(state) && !state["in_game"]
+        return "start silent"
+    end
+    if "game_state" in keys(state)
+        gs = state["game_state"]
+        if gs["screen_type"] == "EVENT"
+            chooseables = filter(c -> "choice_index" in keys(c), gs["screen_state"]["options"])
+            random_event_selection = sample(map(c -> c["choice_index"], chooseables))
+            return "choose $random_event_selection"
+        end
+        if gs["screen_type"] == "MAP"
+            random_map_selection = sample(0:length(gs["choice_list"])-1)
+            return "choose $random_map_selection"
+        end
+        if gs["screen_type"] == "NONE"
+            cs = gs["combat_state"]
+            hand = collect(zip(cs["hand"], 1:100))
+            monsters = collect(zip(cs["monsters"], 0:100))
+            playable_hand = filter(c -> c[1]["is_playable"], hand)
+            if length(playable_hand) == 0
+                return "end"
+            end
+            attackable_monsters = filter(m -> !m[1]["is_gone"], monsters)
+            random_card_to_play = sample(playable_hand)
+            random_card_to_play_index = random_card_to_play[2]
+            if random_card_to_play[1]["has_target"]
+                random_monster_to_attack_index = sample(attackable_monsters)[2]
+                return "play $random_card_to_play_index $random_monster_to_attack_index"
+            end
+            return "play $random_card_to_play_index"
+        end
+        if gs["screen_type"] == "COMBAT_REWARD"
+            if !in("choose", state["available_commands"])
+                return "proceed"
+            end
+            return "choose 0"
+        end
+        if gs["screen_type"] == "CARD_REWARD"
+            if !in("choose", state["available_commands"])
+                return "proceed"
+            end
+            return "choose 0"
+        end
+        if gs["screen_type"] == "HAND_SELECT"
+            if !in("choose", state["available_commands"])
+                return "proceed"
+            end
+            random_card_choice = sample(0:length(gs["choice_list"])-1)
+            return "choose $random_card_choice"
+        end
+        # Peek at the shop to help us learn cards and relics.
+        if gs["screen_type"] == "SHOP_ROOM"
+            if gs["floor"] in shop_floors
+                return "proceed"
+            end
+            return "choose 0"
+        end
+        if gs["screen_type"] == "SHOP_SCREEN"
+            push!(shop_floors, gs["floor"])
+            return "leave"
+        end
+        if gs["screen_type"] == "GRID"
+            if !in("choose", state["available_commands"])
+                return "confirm"
+            end
+            random_choice = sample(0:length(gs["choice_list"])-1)
+            return "choose $random_choice"
+        end
+        if gs["screen_type"] == "GAME_OVER"
+            return "proceed"
+        end
+        if gs["screen_type"] == "REST"
+            if !in("choose", state["available_commands"])
+                return "proceed"
+            end
+            return "choose 1"
+        end
+        if gs["screen_type"] == "CHEST"
+            if !in("choose", state["available_commands"])
+                return "proceed"
+            end
+            return "choose 0"
+        end
+    end
+    nothing
+end
+
+run()
--- repl.jl
+++ repl.jl
@@ -3,10 +3,23 @@ using Sockets
 
 const SOCKET_FILE = "/home/devjac/Code/julia/solve_the_spire/relay_socket"
 
+function hide_map(state)
+    result = deepcopy(state)
+    if "map" in keys(result)
+        result["map"] = "..."
+    end
+    if "game_state" in keys(result)
+        if "map" in keys(result["game_state"])
+            result["game_state"]["map"] = "..."
+        end
+    end
+    result
+end
+
 socket = connect(SOCKET_FILE)
 while true
     sts_state = JSON.parse(readline(socket))
-    JSON.print(stdout, sts_state, 8)
+    JSON.print(stdout, hide_map(sts_state), 12)
     print("Command: ")
     command = readline(stdin)
     write(socket, command * "\n")
