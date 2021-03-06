95ded0f7c4ed470797f58ad54eea82121e6ad78a
DevJac <dbuttonz@gmail.com>
2021-03-05 22:26:35 -0700

    train!

--- src/STSAgents.jl
+++ src/STSAgents.jl
@@ -4,6 +4,8 @@ using Flux
 using Networks
 using SARSM
 using StatsBase
+using Utils
+using Zygote
 
 export action, reward
 export CardPlayingAgent
@@ -14,6 +16,8 @@ struct CardPlayingAgent
     hand_card_encoder
     hand_card_embedder
     hand_card_selector
+    critic_hand_card_embedder
+    critic
     sars
 end
 
@@ -23,10 +27,33 @@ function CardPlayingAgent()
     hand_card_encoder = make_hand_card_encoder(DefaultGameData)
     hand_card_embedder = VanillaNetwork(length(hand_card_encoder), 100, [500])
     hand_card_selector = VanillaNetwork(
-        length(draw_discard_encoder) + length(player_encoder) + length(hand_card_encoder) + 100,
+        length(player_encoder) + length(draw_discard_encoder) + 100 + length(hand_card_encoder),
         1,
         [500, 500, 500])
-    CardPlayingAgent(player_encoder, draw_discard_encoder, hand_card_encoder, hand_card_embedder, hand_card_selector, SARS())
+    critic_hand_card_embedder = VanillaNetwork(length(hand_card_encoder), 100, [500])
+    critic = VanillaNetwork(
+        length(player_encoder) + length(draw_discard_encoder) + 100,
+        1,
+        [500, 500, 500])
+    CardPlayingAgent(
+        player_encoder,
+        draw_discard_encoder,
+        hand_card_encoder,
+        hand_card_embedder,
+        hand_card_selector,
+        critic_hand_card_embedder,
+        critic,
+        SARS())
+end
+
+function action_value(agent::CardPlayingAgent, sts_state)
+    hand = collect(enumerate(sts_state["game_state"]["combat_state"]["hand"]))
+    embedded_cards = map(c -> agent.critic_hand_card_embedder(agent.hand_card_encoder(c[2])), hand)
+    pooled_cards = maximum(reduce(hcat, embedded_cards), dims=2)
+    player_encoded = agent.player_encoder(sts_state)
+    draw_discard_encoded = agent.draw_discard_encoder(sts_state)
+    critic_input = vcat(player_encoded, draw_discard_encoded, pooled_cards)
+    agent.critic(critic_input)
 end
 
 function action_probabilities(agent::CardPlayingAgent, sts_state)
@@ -57,6 +84,47 @@ function reward(agent::CardPlayingAgent, sts_state, continuity=1.0f0)
         last_hp = agent.sars.states[end]["game_state"]["current_hp"]
         current_hp = sts_state["game_state"]["current_hp"]
         add_reward(agent.sars, current_hp - last_hp, continuity)
+    end
+end
+
+function valgrad(f, x...)
+    val, back = pullback(f, x...)
+    val, back(1)
+end
+
+function train!(agent::CardPlayingAgent, epochs=10_000)
+    policy_opt = RMSProp()
+    critic_opt = RMSProp()
+    sars = fill_q(agent.sars)
+    target_agent = deepcopy(agent)
+    for epoch in 1:epochs
+        batch = sample(sars, 100)
+        prms = params(agent.hand_card_embedder, agent.hand_card_selector)
+        loss, grads = valgrad(prms) do
+            -mean(batch) do sar
+                online_aps = action_probabilities(agent, sar.state)
+                online_ap = online_aps[sar.action]
+                target_aps = action_probabilities(target_agent, sar.state)
+                target_ap = target_aps[sar.action]
+                advantage = sar.q - only(action_value(target_agent, sar.state))
+                min(
+                    (online_ap / target_ap) * advantage,
+                    clip(online_ap / target_ap, 0.2) * advantage)
+            end
+        end
+        Flux.Optimise.update!(policy_opt, prms, grads)
+    end
+    for epoch in 1:epochs
+        batch = sample(sars, 100)
+        prms = params(agent.critic_hand_card_embedder, agent.critic)
+        loss, grads = valgrad(prms) do
+            mean(batch) do sar
+                predicted_q = only(action_value(agent, sar.state))
+                actual_q = sar.q
+                (predicted_q - actual_q)^2
+            end
+        end
+        Flux.Optimise.update!(critic_opt, prms, grads)
     end
 end
 
