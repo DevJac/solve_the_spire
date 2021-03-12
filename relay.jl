65337d6730091c21d6fe8d7c858ed1e8fe00df91
DevJac <dbuttonz@gmail.com>
2021-03-12 14:01:53 -0700

    Refactor relay.jl

--- relay.jl
+++ relay.jl
@@ -1,37 +1,33 @@
 using Sockets
 
-const SOCKET_FILE = "/home/devjac/Code/julia/solve_the_spire/relay_socket"
-
-server = listen(SOCKET_FILE)
 println("ready")
-socket = accept(server)
+
+const SOCKET_FILE = ENV["STS_COMMUNICATION_SOCKET"]
+const server = listen(SOCKET_FILE)
+const socket = accept(server)
 
 last_command = time()
 
-sts_to_socket = @async begin
-    while true
-        write(socket, readline(stdin) * "\n")
-    end
-end
-
-socket_to_sts = @async begin
-    while true
-        command = readline(socket)
-        sleep(0.01)
-        global last_command = time()
-        write(stdout, command * "\n")
-    end
-end
-
-state_wakeup = @async begin
+@async begin
     while true
         sleep(10)
-        if last_command + 25 < time()
+        if last_command + 10 < time()
             write(stdout, "state\n")
         end
     end
 end
 
-wait(sts_to_socket)
-wait(socket_to_sts)
-wait(state_wakeup)
+@async begin
+    while true
+        write(socket, readline(stdin) * "\n")
+    end
+end
+
+wait(@async begin
+    while true
+        command = readline(socket)
+        if command == ""; break end
+        global last_command = time()
+        write(stdout, command * "\n")
+    end
+end)
