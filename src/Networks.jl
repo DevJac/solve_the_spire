02424e2407c5ec2538213b97201b9bfe796b7ded
DevJac <dbuttonz@gmail.com>
2021-03-21 12:21:00 -0600

    PoolNetwork

--- src/Networks.jl
+++ src/Networks.jl
@@ -1,7 +1,7 @@
 module Networks
 using Flux
 using Statistics
-export PolicyNetwork, QNetwork, VanillaNetwork, value, advantage
+export PolicyNetwork, QNetwork, VanillaNetwork, PoolNetwork, value, advantage
 
 #####################
 # Utility Functions #
@@ -90,5 +90,38 @@ end
 (n::VanillaNetwork)(s) = n.network(s)
 
 Base.length(n::VanillaNetwork) = length(n.network.layers[end].b)
+
+################
+# Pool Network #
+################
+
+
+struct PoolNetwork{N}
+    network     :: N
+    oif_weights :: Array{Float32,2}
+end
+
+Flux.@functor PoolNetwork
+
+function PoolNetwork(in, out, hidden, activation=relu, initW=Flux.kaiming_uniform)
+    hidden = vcat(in, hidden)
+    layers = Any[]
+    for i in 1:length(hidden)-1
+        push!(layers, Dense(hidden[i], hidden[i+1], activation, initW=initW))
+    end
+    push!(layers, Dense(hidden[end], out, identity))
+    PoolNetwork(Chain(layers...), ones(Float32, out, 4))
+end
+
+function (n::PoolNetwork)(s)
+    # network out
+    no = n.network(s)
+    # applied order invariant functions
+    applied_oif = hcat(sum(no, dims=2), mean(no, dims=2), minimum(no, dims=2), maximum(no, dims=2))
+    pooled = sum(applied_oif .* n.oif_weights, dims=2)
+    reshape(pooled, length(pooled))
+end
+
+Base.length(n::PoolNetwork) = length(n.network.layers[end].b)
 
 end # module
--- test/Networks.jl
+++ test/Networks.jl
@@ -66,3 +66,24 @@ end
     @test size(n(rand(4, 10))) == (4, 10)
     @test length(n) == 4
 end
+
+@testset "PoolNetwork" begin
+    n = PoolNetwork(4, 3, [8, 9])
+    @test params(n) .|> length == [32, 8, 72, 9, 27, 3, 12]
+    @test size(n(rand(4, 10))) == (3,)
+    @test length(n) == 3
+    n = PoolNetwork(4, 3, [8])
+    @test params(n) .|> length == [32, 8, 24, 3, 12]
+    @test size(n(rand(4, 10))) == (3,)
+    n = PoolNetwork(4, 3, [])
+    @test params(n) .|> length == [12, 3, 12]
+    @test size(n(rand(4, 10))) == (3,)
+    hidden_layers = [8, 9]
+    n = PoolNetwork(4, 3, hidden_layers)
+    @test params(n) .|> length == [32, 8, 72, 9, 27, 3, 12]
+    @test size(n(rand(4, 10))) == (3,)
+    n = PoolNetwork(4, 4, hidden_layers)
+    @test params(n) .|> length == [32, 8, 72, 9, 36, 4, 16]
+    @test size(n(rand(4, 10))) == (4,)
+    @test length(n) == 4
+end
