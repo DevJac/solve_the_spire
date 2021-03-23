7e7d1e62803136829ffd95b909945951d23bf848
DevJac <dbuttonz@gmail.com>
2021-03-23 16:25:07 -0600

    More Zygote.ignore

--- src/Networks.jl
+++ src/Networks.jl
@@ -136,9 +136,9 @@ function (n::PoolNetwork)(s)
     # network out
     no = n.network(s)
     # applied order invariant functions
-    applied_oif = hcat(sum(no, dims=2), mean(no, dims=2), minimum(no, dims=2), maximum(no, dims=2))
+    applied_oif = Zygote.@ignore hcat(sum(no, dims=2), mean(no, dims=2), minimum(no, dims=2), maximum(no, dims=2))
     pooled = sum(applied_oif .* n.oif_weights, dims=2)
-    reshape(pooled, length(pooled))
+    Zygote.@ignore reshape(pooled, length(pooled))
 end
 
 Base.length(n::PoolNetwork) = length(n.network.layers[end].b)
