168b27c74bcf1d99ad247b6689ef09f6f43e369e
DevJac <dbuttonz@gmail.com>
2021-03-27 16:34:58 -0600

    PoolEachNetwork

--- src/Networks.jl
+++ src/Networks.jl
@@ -2,7 +2,7 @@ module Networks
 using Flux
 using Statistics
 using Zygote
-export PolicyNetwork, QNetwork, VanillaNetwork, PoolNetwork, value, advantage
+export PolicyNetwork, QNetwork, VanillaNetwork, PoolNetwork, PoolEachNetwork, value, advantage
 
 #####################
 # Utility Functions #
@@ -79,6 +79,7 @@ end
 Flux.@functor VanillaNetwork
 
 function VanillaNetwork(in, out, hidden, activation=relu, initW=Flux.kaiming_uniform)
+    in += 1
     hidden = vcat(in, hidden)
     layers = Any[]
     for i in 1:length(hidden)-1
@@ -91,20 +92,19 @@ end
 function (n::VanillaNetwork)(f, s)
     if isempty(s)
         in = size(n.network.layers[1].W, 2)
-        n(zeros(in))
+        n.network(zeros(in))
     else
         n(reduce(hcat, map(f, s)))
     end
 end
 
-(n::VanillaNetwork)(s) = n.network(s)
+(n::VanillaNetwork)(s) = n.network([1; s])
 
 Base.length(n::VanillaNetwork) = length(n.network.layers[end].b)
 
 ################
 # Pool Network #
 ################
-
 
 struct PoolNetwork{N}
     network     :: N
@@ -114,6 +114,7 @@ end
 Flux.@functor PoolNetwork
 
 function PoolNetwork(in, out, hidden, activation=relu, initW=Flux.kaiming_uniform)
+    in += 1
     hidden = vcat(in, hidden)
     layers = Any[]
     for i in 1:length(hidden)-1
@@ -126,7 +127,7 @@ end
 function (n::PoolNetwork)(f, s)
     if isempty(s)
         in = size(n.network.layers[1].W, 2)
-        n(zeros(in))
+        n.network(zeros(in))
     else
         n(reduce(hcat, map(f, s)))
     end
@@ -134,7 +135,7 @@ end
 
 function (n::PoolNetwork)(s)
     # network out
-    no = n.network(s)
+    no = n.network([1; s])
     # applied order invariant functions
     applied_oif = hcat(sum(no, dims=2), mean(no, dims=2), minimum(no, dims=2), maximum(no, dims=2))
     pooled = sum(applied_oif .* n.oif_weights, dims=2)
@@ -142,5 +143,39 @@ function (n::PoolNetwork)(s)
 end
 
 Base.length(n::PoolNetwork) = length(n.network.layers[end].b)
+
+#####################
+# Pool Each Network #
+#####################
+
+struct PoolEachNetwork{N,M}
+    each_network :: N
+    pool_network :: M
+end
+
+Flux.@functor PoolEachNetwork
+
+function PoolEachNetwork(in, out, hidden, activation=relu, initW=Flux.kaiming_uniform)
+    out = Int(ceil(out/2))
+    PoolEachNetwork(
+        VanillaNetwork(in, out, hidden, activation, initW),
+        PoolNetwork(in, out, hidden, activation, initW))
+end
+
+function (n::PoolEachNetwork)(f, s)
+    each = n.each_network(f, s)
+    vcat(
+        each,
+        repeat(n.pool_network(f, s), 1, size(each, 2)))
+end
+
+function (n::PoolEachNetwork)(s)
+    each = n.each_network(s)
+    vcat(
+        each,
+        repeat(n.pool_network(s), 1, size(each, 2)))
+end
+
+Base.length(n::PoolEachNetwork) = length(n.each_network) * 2
 
 end # module
