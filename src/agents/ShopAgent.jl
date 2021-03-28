9db916a5b4acefa448012edbbcf767c0ce1c6dfe
DevJac <dbuttonz@gmail.com>
2021-03-27 21:01:59 -0600

    ShopAgent WIP

--- src/agents/ShopAgent.jl
+++ src/agents/ShopAgent.jl
@@ -5,12 +5,9 @@ mutable struct ShopAgent
     deck_embedder
     potions_embedder
     relics_embedder
-    all_card_choice_embedder
-    single_card_choice_embedder
-    all_relic_choice_embedder
-    single_relic_choice_embedder
-    all_potion_choice_embedder
-    single_potion_choice_embedder
+    card_choice_embedder
+    relic_choice_embedder
+    potion_choice_embedder
     policy
     critic
     policy_opt
@@ -23,24 +20,18 @@ function ShopAgent()
     deck_embedder = PoolNetwork(length(card_encoder), 20, [50])
     potions_embedder = VanillaNetwork(length(potions_encoder), 20, [50])
     relics_embedder = VanillaNetwork(length(relics_encoder), 20, [50])
-    all_card_choice_embedder = PoolNetwork(legnth(card_encoder)+1, 20, [50])
-    single_card_choice_embedder = VanillaNetwork(length(card_encoder)+1, 20, [50])
-    all_relic_choice_embedder = VanillaNetwork(length(relics_encoder), 20, [50])
-    single_relic_choice_embedder = VanillaNetwork(length(relics_encoder)+1, 20, [50])
-    all_potion_choice_embedder = VanillaNetwork(length(potions_encoder), 20, [50])
-    single_potion_choice_embedder = VanillaNetwork(length(potions_encoder)+1, 20, [50])
+    card_choice_embedder = PoolNetwork(legnth(card_encoder)+1, 20, [50])
+    relic_choice_embedder = PoolNetwork(length(relics_encoder)+1, 20, [50])
+    potion_choice_embedder = PoolNetwork(length(potions_encoder)+1, 20, [50])
     policy = VanillaNetwork(
         sum(length, [
             player_embedder,
             deck_embedder,
             potions_embedder,
             relics_embedder,
-            all_card_choice_embedder,
-            single_card_choice_embedder,
-            all_relic_choice_embedder,
-            single_relic_choice_embedder,
-            all_potion_choice_embedder,
-            single_potion_choice_embedder]),
+            card_choice_embedder,
+            relic_choice_embedder,
+            potion_choice_embedder])
         1, [200, 50, 50])
     critic = VanillaNetwork(
         sum(length, [
@@ -48,40 +39,39 @@ function ShopAgent()
             deck_embedder,
             potions_embedder,
             relics_embedder,
-            all_card_choice_embedder,
-            all_relic_choice_embedder,
-            all_potion_choice_embedder]),
+            pool(card_choice_embedder),
+            pool(relic_choice_embedder),
+            pool(potion_choice_embedder)]),
         1, [200, 50, 50])
     policy_opt = ADADelta()
     critic_opt = ADADelta()
     sars = SARS()
     last_visited_shop_floor = nothing
     ShopAgent(
-        player_embedder
-        deck_embedder
-        potions_embedder
-        relics_embedder
-        all_card_choice_embedder
-        single_card_choice_embedder
-        all_relic_choice_embedder
-        single_relic_choice_embedder
-        all_potion_choice_embedder
-        single_potion_choice_embedder
-        policy
-        critic
-        policy_opt
-        critic_opt
-        sars
-        last_visited_shop_floor)
+        player_embedder,
+        deck_embedder,
+        potions_embedder,
+        relics_embedder,
+        card_choice_embedder,
+        relic_choice_embedder,
+        potion_choice_embedder,
+        policy,
+        critic,
+        policy_opt,
+        critic_opt,
+        sars,
+        last_visited_shop_floor :: Union{Nothing, Int})
 end
 
 function action_probabilities(agent::ShopAgent, ra::RootAgent, sts_state)
     gs = sts_state["game_state"]
-    local cards_offered
-    local relics_offered
-    local potions_offered
+    local cards_offered = []
+    local relics_offered = []
+    local potions_offered = []
     local purge_offered = false
     local purge_cost = 0
+    local chest_available = false
+    local saphire_key = false
     Zygote.ignore() do
         if gs["screen_type"] == "SHOP_SCREEN"
             cards_offered = gs["screen_state"]["cards"]
@@ -90,7 +80,33 @@ function action_probabilities(agent::ShopAgent, ra::RootAgent, sts_state)
             purge_offered = gs["screen_state"]["purge_available"]
             purge_cost = gs["screen_state"]["purge_cost"]
         end
+        if gs["screen_type"] == "BOSS_REWARD"
+            relics_offered = gs["screen_state"]["relics"]
+        end
+        if gs["screen_type"] == "CHEST"
+            chest_available = true
+        end
+        if gs["screen_type"] == "COMBAT_REWARD"
+            relics_offered = map(x -> x["relic"], filter(x -> x["reward_type"] == "RELIC", gs["screen_state"]["rewards"]))
+            potions_offered = map(x -> x["potion"], filter(x -> x["reward_type"] == "POTION", gs["screen_state"]["rewards"]))
+            saphire_key = any(x -> x["reward_type"] == "SAPHIRE_KEY", gs["screen_state"]["rewards"])
+        end
+        function populate_cost(x)
+            x["cost"] = get(x, "cost", 0)
+            x
+        end
+        map(populate_cost, cards_offered)
+        map(populate_cost, relics_offered)
+        map(populate_cost, potions_offered)
     end
+    player_e = agent.player_embedder(player_basic_encoder(sts_state))
+    deck_e = agent.deck_embedder(card_encoder, gs["deck"])
+    potions_e = agent.potions_embedder(potions_encoder(gs["potions"]))
+    relics_e = agent.relics_embedder(relics_encoder(gs["relics"]))
+    card_choice_e = agent.card_choice_embedder(c -> [card_encoder(c); c["cost"]], cards_offered)
+    relic_choice_e = agent.relic_choice_embedder(r -> [relics_encoder([r]); r["cost"]], relics_offered)
+    potion_choice_e = agent.potion_choice_embedder(p -> [potions_encoder([p]); r["cost"]], potions_offered)
+    policy
 end
 
 
@@ -114,6 +130,28 @@ function action(agent::ShopAgent, ra::RootAgent, sts_state)
             end
             random_shop_choice = sample(0:length(gs["choice_list"])-1)
             return "choose $random_shop_choice"
+        end
+    end
+    if "game_state" in keys(sts_state)
+        gs = sts_state["game_state"]
+        if gs["screen_type"] == "COMBAT_REWARD"
+            if !in("choose", sts_state["available_commands"])
+                return "proceed"
+            end
+            if gs["screen_state"]["rewards"][1]["reward_type"] == "POTION" && all(p -> p["id"] != "Potion Slot", gs["potions"])
+                return "proceed"
+            end
+            return "choose 0"
+        end
+        if gs["screen_type"] == "CHEST"
+            if !in("choose", sts_state["available_commands"])
+                return "proceed"
+            end
+            return "choose 0"
+        end
+        if gs["screen_type"] == "BOSS_REWARD"
+            random_choice = sample(0:2)
+            return "choose $random_choice"
         end
     end
 end
