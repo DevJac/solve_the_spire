75eb18ab2cbe68df410500f170712a9be0cfe000
DevJac <dbuttonz@gmail.com>
2021-04-03 13:51:08 -0600

    ChoiceEncoder params and encode_state

--- src/ChoiceEncoders.jl
+++ src/ChoiceEncoders.jl
@@ -1,4 +1,6 @@
 module ChoiceEncoders
+using Flux
+using Networks
 using Utils
 using Zygote
 
@@ -12,14 +14,39 @@ struct ChoiceEncoder
     choices_encoded  :: Dict{Symbol, Vector{Any}}
     choice_actions   :: Dict{Symbol, Vector{Any}}
 end
-function ChoiceEncoder(state_embedders, choice_embedders, pooler)
+
+function ChoiceEncoder(state_embedders, choice_embedders, pool_out, pool_hidden)
     ChoiceEncoder(
         state_embedders,
         choice_embedders,
-        pooler,
+        PoolNetwork(sum(length, values(choice_embedders)), pool_out, pool_hidden),
         Dict(k => nothing for k in keys(state_embedders)),
         Dict(k => [] for k in keys(choice_embedders)),
         Dict(k => [] for k in keys(choice_embedders)))
+end
+
+function ChoiceEncoder(state_embedders, choice_embedders, pool)
+    ChoiceEncoder(
+        state_embedders,
+        choice_embedders,
+        pool,
+        Dict(k => nothing for k in keys(state_embedders)),
+        Dict(k => [] for k in keys(choice_embedders)),
+        Dict(k => [] for k in keys(choice_embedders)))
+end
+
+function reset!(ce::ChoiceEncoder)
+    ce.states_encoded = Dict(k => nothing for k in keys(ce.state_embedders))
+    ce.choices_encoded = Dict(k => [] for k in keys(ce.choice_embedders))
+    ce.choie_actions = Dict(k => [] for k in keys(ce.choice_embedders))
+end
+
+function Flux.trainable(ce::ChoiceEncoder)
+    trainables = []
+    append!(trainables, [Flux.trainable(n) for n in values(ce.state_embedders)])
+    append!(trainables, [Flux.trainable(n) for n in values(ce.choice_embedders)])
+    push!(trainables, Flux.trainable(ce.pooler))
+    tuple(trainables...)
 end
 
 function Base.length(ce::ChoiceEncoder)
@@ -69,6 +96,33 @@ function encode_choices(ce::ChoiceEncoder)
         choices_embedded)
     Zygote.@ignore @assert size(final_encoding, 2) == length(choice_actions)
     final_encoding, choice_actions
+end
+
+function encode_state(ce::ChoiceEncoder)
+    if any(isnothing, values(ce.states_encoded))
+        throw("Missing encoded states")
+    end
+    state_keys = Zygote.@ignore sort(collect(keys(ce.state_embedders)))
+    choice_keys = Zygote.@ignore sort(collect(keys(ce.choice_embedders)))
+    presence_indicators = length(ce.choice_embedders) > 1 ? length(ce.choice_embedders) : 0
+    state_embedded = reduce(
+        vcat,
+        [ce.state_embedders[sym](ce.states_encoded[sym]) for sym in state_keys])
+    Zygote.@ignore @assert length(state_embedded) == sum(length, values(ce.state_embedders))
+    choice_categories_embedded = [
+        length(ce.choices_encoded[sym]) >= 1 ? reduce(hcat, ce.choice_embedders[sym].(ce.choices_encoded[sym])) : mask(ce, sym)
+        for sym in choice_keys]
+    if presence_indicators > 0
+        choices_embedded = reduce(diagcat, map(onetop, choice_categories_embedded))
+    else
+        choices_embedded = reduce(diagcat, choice_categories_embedded)
+    end
+    choice_actions = reduce(vcat, [ce.choice_actions[sym] for sym in choice_keys])
+    Zygote.@ignore @assert size(choices_embedded, 2) == length(choice_actions)
+    choices_embedded = choices_embedded[:, findall(a -> !isa(a, MaskAction), choice_actions)]
+    choice_actions = choice_actions[findall(a -> !isa(a, MaskAction), choice_actions)]
+    pool = ce.pooler(choices_embedded)
+    vcat(state_embedded, pool)
 end
 
 struct MaskAction; end
--- test/ChoiceEncoders.jl
+++ test/ChoiceEncoders.jl
@@ -1,4 +1,6 @@
 using Test
+using Flux
+using Networks
 
 using ChoiceEncoders
 
@@ -117,4 +119,39 @@ end
                                     4
                                     4
                                     4]), 23, 1)
+end
+
+@testset "params" begin
+    n = VanillaNetwork(4, 3, [8])
+    ce = ChoiceEncoder(
+        Dict(:state_a => VanillaNetwork(4, 3, [8]), :state_b => VanillaNetwork(4, 3, [8])),
+        Dict(:choice_a => VanillaNetwork(4, 3, [8]), :choice_b => VanillaNetwork(4, 3, [8])),
+        3, [8])
+    @test length(params(ce)) == 21
+    @test length.(params(ce)) = [32, 8, 24, 3, 32, 8, 24, 3, 32, 8, 24, 3, 32, 8, 24, 3, 32, 8, 24, 3, 12]
+end
+
+@testset "encode_state" begin
+    ce = ChoiceEncoder(
+        Dict(:state_a => MockNetwork(3, 2, 1), :state_b => MockNetwork(4, 3, 2)),
+        Dict(:choice_a => MockNetwork(5, 4, 3), :choice_b => MockNetwork(6, 5, 4)),
+        MockNetwork(11, 7, 5, true))
+    add_state(ce, :state_a, rand(3))
+    add_state(ce, :state_b, rand(4))
+    add_choice(ce, :choice_a, rand(5), 1)
+    add_choice(ce, :choice_a, rand(5), 2)
+    add_choice(ce, :choice_b, rand(6), 3)
+    r = encode_state(ce)
+    @test r == Float32.([1
+                         1
+                         2
+                         2
+                         2
+                         5
+                         5
+                         5
+                         5
+                         5
+                         5
+                         5])
 end
