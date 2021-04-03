b7a56d07c8ee8f8cb880231239092dc853e00480
DevJac <dbuttonz@gmail.com>
2021-04-03 12:22:49 -0600

    Remove most Networks

--- src/Networks.jl
+++ src/Networks.jl
@@ -4,70 +4,6 @@ using Statistics
 using Zygote
 export PolicyNetwork, QNetwork, VanillaNetwork, PoolNetwork, PoolEachNetwork, GRUNetwork, value, advantage
 
-#####################
-# Utility Functions #
-#####################
-
-value(network, s) = mean(network(s), dims=1)
-
-function advantage(network, s)
-    q = network(s)
-    q .- mean(q, dims=1)
-end
-
-##################
-# Policy Network #
-##################
-
-struct PolicyNetwork{N}
-    network :: N
-end
-
-Flux.@functor PolicyNetwork
-
-function PolicyNetwork(in, out, hidden, activation=relu, initW=Flux.kaiming_uniform)
-    hidden = vcat(in, hidden)
-    layers = Any[]
-    for i in 1:length(hidden)-1
-        push!(layers, Dense(hidden[i], hidden[i+1], activation, initW=initW))
-    end
-    push!(layers, Dense(hidden[end], out, identity))
-    push!(layers, softmax)
-    PolicyNetwork(Chain(layers...))
-end
-
-(p::PolicyNetwork)(s) = p.network(s)
-
-#############
-# Q Network #
-#############
-
-struct QNetwork{T, A, V}
-    trunk_network  :: T
-    action_network :: A
-    value_network  :: V
-    function QNetwork(in, out, hidden, activation=relu, initW=Flux.kaiming_uniform)
-        hidden = vcat(in, hidden)
-        layers = Any[]
-        for i in 1:length(hidden)-1
-            push!(layers, Dense(hidden[i], hidden[i+1], activation, initW=initW))
-        end
-        t = Chain(layers...)
-        a = Dense(hidden[end], out, identity)
-        v = Dense(hidden[end], 1, identity)
-        new{typeof(t), typeof(a), typeof(v)}(t, a, v)
-    end
-end
-
-Flux.@functor QNetwork
-
-function (q::QNetwork)(s)
-    t = q.trunk_network(s)
-    a = q.action_network(t)
-    v = q.value_network(t)
-    v .+ a .- mean(a, dims=1)
-end
-
 ###################
 # Vanilla Network #
 ###################
@@ -79,7 +15,6 @@ end
 Flux.@functor VanillaNetwork
 
 function VanillaNetwork(in, out, hidden, activation=relu, initW=Flux.kaiming_uniform)
-    in += 1
     hidden = vcat(in, hidden)
     layers = Any[]
     for i in 1:length(hidden)-1
@@ -89,17 +24,7 @@ function VanillaNetwork(in, out, hidden, activation=relu, initW=Flux.kaiming_uni
     VanillaNetwork(Chain(layers...))
 end
 
-function (n::VanillaNetwork)(f, s)
-    if isempty(s)
-        null(n)
-    else
-        n(reduce(hcat, map(f, s)))
-    end
-end
-
-(n::VanillaNetwork)(s) = n.network([1; s])
-
-null(n::VanillaNetwork) = n.network(zeros(size(n.network.layers[1].W, 2)))
+(n::VanillaNetwork)(s) = n.network(s)
 
 Base.length(n::VanillaNetwork) = length(n.network.layers[end].b)
 
@@ -115,7 +40,6 @@ end
 Flux.@functor PoolNetwork
 
 function PoolNetwork(in, out, hidden, activation=relu, initW=Flux.kaiming_uniform)
-    in += 1
     hidden = vcat(in, hidden)
     layers = Any[]
     for i in 1:length(hidden)-1
@@ -125,102 +49,15 @@ function PoolNetwork(in, out, hidden, activation=relu, initW=Flux.kaiming_unifor
     PoolNetwork(Chain(layers...), ones(Float32, out, 4))
 end
 
-function (n::PoolNetwork)(f, s)
-    if isempty(s)
-        null(n)
-    else
-        n(reduce(hcat, map(f, s)))
-    end
-end
-
 function (n::PoolNetwork)(s)
     # network out
-    no = n.network([1; s])
+    no = n.network(s)
     # applied order invariant functions
     applied_oif = hcat(sum(no, dims=2), mean(no, dims=2), minimum(no, dims=2), maximum(no, dims=2))
     pooled = sum(applied_oif .* n.oif_weights, dims=2)
     reshape(pooled, length(pooled))
 end
 
-null(n::PoolNetwork) = n.network(zeros(size(n.network.layers[1].W, 2)))
-
 Base.length(n::PoolNetwork) = length(n.network.layers[end].b)
-
-#####################
-# Pool Each Network #
-#####################
-
-struct PoolEachNetwork{N,M}
-    each_network :: N
-    pool_network :: M
-end
-
-Flux.@functor PoolEachNetwork
-
-pool(n::PoolEachNetwork) = n.pool_network
-
-function PoolEachNetwork(in, out, hidden, activation=relu, initW=Flux.kaiming_uniform)
-    PoolEachNetwork(
-        VanillaNetwork(in, out, hidden, activation, initW),
-        PoolNetwork(in, out, hidden, activation, initW))
-end
-
-function (n::PoolEachNetwork)(f, s)
-    each = n.each_network(f, s)
-    vcat(
-        each,
-        repeat(n.pool_network(f, s), 1, size(each, 2)))
-end
-
-function (n::PoolEachNetwork)(s)
-    each = n.each_network(s)
-    vcat(
-        each,
-        repeat(n.pool_network(s), 1, size(each, 2)))
-end
-
-function null(n::PoolEachNetwork)
-    each = null(n.each_network)
-    vcat(
-        each,
-        repeat(null(n.pool_network), 1, size(each, 2)))
-end
-
-Base.length(n::PoolEachNetwork) = length(n.each_network) * 2
-
-###############
-# GRU Network #
-###############
-
-struct GRUNetwork{N}
-    network :: N
-end
-
-Flux.@functor GRUNetwork
-
-function GRUNetwork(in, out, hidden)
-    in += 1
-    hidden = vcat(in, hidden)
-    layers = Any[]
-    for i in 1:length(hidden)-1
-        push!(layers, GRU(hidden[i], hidden[i+1]))
-    end
-    push!(layers, GRU(hidden[end], out))
-    GRUNetwork(Chain(layers...))
-end
-
-function (n::GRUNetwork)(f, s)
-    if isempty(s)
-        null(n)
-    else
-        n(reduce(hcat, map(f, s)))
-    end
-end
-
-(n::GRUNetwork)(s) = n.network([1; s])
-
-null(n::GRUNetwork) = n.network(zeros(size(n.network.layers[1].W, 2)))
-
-Base.length(n::GRUNetwork) = length(n.network.layers[end].cell.h)
 
 end # module
--- test/Networks.jl
+++ test/Networks.jl
@@ -3,99 +3,44 @@ using Test
 
 using Networks
 
-@testset "value" begin
-    q = QNetwork(4, 3, [])
-    i = rand(4, 10)
-    @test size(q(i)) == (3, 10)
-    @test size(value(q, i)) == (1, 10)
-end
-
-@testset "advantage" begin
-    p = PolicyNetwork(4, 3, [8, 9])
-    result = advantage(p, rand(4, 10))
-    @test size(result) == (3, 10)
-    @test isapprox(sum(result, dims=1), zeros(1, 10), atol=1e-5)
-end
-
-@testset "PolicyNetwork" begin
-    p = PolicyNetwork(4, 3, [8, 9])
-    @test params(p) .|> length == [32, 8, 72, 9, 27, 3]
-    result = p(rand(4, 10))
-    @test size(result) == (3, 10)
-    @test all(result) do n
-        0 <= n && n <= 1
-    end
-    @test isapprox(sum(result, dims=1), ones(1, 10))
-    hidden_layers = [8, 9]
-    p = PolicyNetwork(4, 3, hidden_layers)
-    @test params(p) .|> length == [32, 8, 72, 9, 27, 3]
-    p = PolicyNetwork(4, 3, hidden_layers)
-    @test params(p) .|> length == [32, 8, 72, 9, 27, 3]
-end
-
-@testset "QNetwork" begin
-    q = QNetwork(4, 3, [8, 9])
-    @test params(q) .|> length == [32, 8, 72, 9, 27, 3, 9, 1]
-    @test size(q(rand(4, 10))) == (3, 10)
-    hidden_layers = [8, 9]
-    q = QNetwork(4, 3, hidden_layers)
-    @test params(q) .|> length == [32, 8, 72, 9, 27, 3, 9, 1]
-    @test size(q(rand(4, 10))) == (3, 10)
-    q = QNetwork(4, 3, hidden_layers)
-    @test params(q) .|> length == [32, 8, 72, 9, 27, 3, 9, 1]
-    @test size(q(rand(4, 10))) == (3, 10)
-end
-
 @testset "VanillaNetwork" begin
     n = VanillaNetwork(4, 3, [8, 9])
-    @test params(n) .|> length == [40, 8, 72, 9, 27, 3]
+    @test params(n) .|> length == [32, 8, 72, 9, 27, 3]
     @test size(n(rand(4, 10))) == (3, 10)
     @test length(n) == 3
     n = VanillaNetwork(4, 3, [8])
-    @test params(n) .|> length == [40, 8, 24, 3]
+    @test params(n) .|> length == [32, 8, 24, 3]
     @test size(n(rand(4, 10))) == (3, 10)
     n = VanillaNetwork(4, 3, [])
-    @test params(n) .|> length == [15, 3]
+    @test params(n) .|> length == [12, 3]
     @test size(n(rand(4, 10))) == (3, 10)
     hidden_layers = [8, 9]
     n = VanillaNetwork(4, 3, hidden_layers)
-    @test params(n) .|> length == [40, 8, 72, 9, 27, 3]
+    @test params(n) .|> length == [32, 8, 72, 9, 27, 3]
     @test size(n(rand(4, 10))) == (3, 10)
     n = VanillaNetwork(4, 4, hidden_layers)
-    @test params(n) .|> length == [40, 8, 72, 9, 36, 4]
+    @test params(n) .|> length == [32, 8, 72, 9, 36, 4]
     @test size(n(rand(4, 10))) == (4, 10)
     @test length(n) == 4
 end
 
 @testset "PoolNetwork" begin
     n = PoolNetwork(4, 3, [8, 9])
-    @test params(n) .|> length == [40, 8, 72, 9, 27, 3, 12]
+    @test params(n) .|> length == [32, 8, 72, 9, 27, 3, 12]
     @test size(n(rand(4, 10))) == (3,)
     @test length(n) == 3
     n = PoolNetwork(4, 3, [8])
-    @test params(n) .|> length == [40, 8, 24, 3, 12]
+    @test params(n) .|> length == [32, 8, 24, 3, 12]
     @test size(n(rand(4, 10))) == (3,)
     n = PoolNetwork(4, 3, [])
-    @test params(n) .|> length == [15, 3, 12]
+    @test params(n) .|> length == [12, 3, 12]
     @test size(n(rand(4, 10))) == (3,)
     hidden_layers = [8, 9]
     n = PoolNetwork(4, 3, hidden_layers)
-    @test params(n) .|> length == [40, 8, 72, 9, 27, 3, 12]
+    @test params(n) .|> length == [32, 8, 72, 9, 27, 3, 12]
     @test size(n(rand(4, 10))) == (3,)
     n = PoolNetwork(4, 4, hidden_layers)
-    @test params(n) .|> length == [40, 8, 72, 9, 36, 4, 16]
+    @test params(n) .|> length == [32, 8, 72, 9, 36, 4, 16]
     @test size(n(rand(4, 10))) == (4,)
     @test length(n) == 4
-end
-
-@testset "PoolEachNetwork" begin
-    n = PoolEachNetwork(5, 2, [8, 9])
-    @test length(n) == 4
-    @test params(n) .|> length == [48, 8, 72, 9, 18, 2, 48, 8, 72, 9, 18, 2, 8]
-    out = n(rand(5, 10))
-    @test size(out) == (4, 10)
-    @test !all(x -> out[1,1] == x, out[1,:])
-    @test !all(x -> out[2,1] == x, out[2,:])
-    @test all(x -> out[3,1] == x, out[3,:])
-    @test all(x -> out[4,1] == x, out[4,:])
 end
