0234288354b7d6d1114f57de1a7e9e1e450b3538
DevJac <dbuttonz@gmail.com>
2021-05-07 22:12:48 -0600

    fill_q takes a function to calculate advantage

--- src/SARSM.jl
+++ src/SARSM.jl
@@ -48,16 +48,18 @@ end
 export SingleSAR, fill_q
 
 struct SingleSAR{S, A}
-    state      :: S
-    action     :: A
-    reward     :: Float32
-    continuity :: Float32
-    q          :: Float32
-    q_norm     :: Float32
-    weight     :: Float32
+    state          :: S
+    action         :: A
+    reward         :: Float32
+    continuity     :: Float32
+    q              :: Float32
+    q_norm         :: Float32
+    advantage      :: Float32
+    advantage_norm :: Float32
+    weight         :: Float32
 end
 
-function fill_q(sars::SARS, discount_factor=1.0f0, episode_continuity_threshold=0.1)
+function fill_q(sars::SARS, advantage, discount_factor=1.0f0, episode_continuity_threshold=0.1)
     @assert all(v -> length(sars.states) == length(v), (sars.actions, sars.rewards))
     first_pass = SingleSAR[]
     q = 0.0f0
@@ -69,12 +71,18 @@ function fill_q(sars::SARS, discount_factor=1.0f0, episode_continuity_threshold=
         if continuity <= episode_continuity_threshold
             episode += 1
         end
-        push!(first_pass, SingleSAR(sars.states[i], sars.actions[i], reward, continuity, q, 0f0, episode))
+        push!(first_pass, SingleSAR(sars.states[i], sars.actions[i], reward, continuity, q, 0f0, 0f0, 0f0, episode))
     end
-    q_mean = mean([sar.q for sar in first_pass])
-    q_std = std([sar.q for sar in first_pass])
+    second_pass = map(first_pass) do sar
+        a = Float32(advantage(sar))
+        SingleSAR(sar.state, sar.action, sar.reward, sar.continuity, sar.q, sar.q_norm, a, 0f0, sar.weight)
+    end
+    q_mean = mean([sar.q for sar in second_pass])
+    q_std = std([sar.q for sar in second_pass])
+    advantage_mean = mean([sar.advantage for sar in second_pass])
+    advantage_std = std([sar.advantage for sar in second_pass])
     episode_counts = Dict{Float32, Int}()
-    map(first_pass) do sar
+    map(second_pass) do sar
         episode_counts[sar.weight] = get(episode_counts, sar.weight, 0) + 1
     end
     reverse([
@@ -85,8 +93,10 @@ function fill_q(sars::SARS, discount_factor=1.0f0, episode_continuity_threshold=
             sar.continuity,
             sar.q,
             ((sar.q - q_mean) / q_std),
+            sar.advantage,
+            ((sar.advantage - advantage_mean) / advantage_std),
             1f0 / episode_counts[sar.weight])
-        for sar in first_pass])
+        for sar in second_pass])
 end
 
 end # module
--- test/SARSM.jl
+++ test/SARSM.jl
@@ -40,13 +40,30 @@ using SARSM
     add_action(sars, 1)
     add_reward(sars, 10)
 
-    sar_structs = fill_q(sars)
+    sar_structs = fill_q(sars, sar -> sar.q)
     @test map(x -> x.q, sar_structs) == Float32.([2, 1, 1, 7, 13, 12, 11, 10])
     @test map(x -> x.continuity, sar_structs) == Float32.([1, 0, 0, 0, 1, 1, 1, 1])
     @test map(x -> x.weight, sar_structs) == Float32.([1/2, 1/2, 1, 1, 1/4, 1/4, 1/4, 1/4])
     @test isapprox(sar_structs[1].q_norm, -1.00268)
+    @test isapprox(sar_structs[1].advantage_norm, -1.00268)
+
+    sar_structs = fill_q(sars, sar -> sar.q + 1)
+    @test map(x -> x.q, sar_structs) == Float32.([2, 1, 1, 7, 13, 12, 11, 10])
+    @test map(x -> x.advantage, sar_structs) == Float32.([3, 2, 2, 8, 14, 13, 12, 11])
+    @test isapprox(sar_structs[1].q_norm, -1.00268)
+    @test isapprox(sar_structs[1].advantage_norm, -1.00268)
+    sar_structs = fill_q(sars, sar -> sar.q * 2)
+    @test isapprox(sar_structs[1].q_norm, -1.00268)
+    @test isapprox(sar_structs[1].advantage_norm, -1.00268)
+    sar_structs = fill_q(sars, sar -> sar.q * 2)
+    @test isapprox(sar_structs[1].q_norm, -1.00268)
+    @test isapprox(sar_structs[1].advantage_norm, -1.00268)
+    sar_structs = fill_q(sars, sar -> sar.q + randn())
+    @test isapprox(sar_structs[1].q_norm, -1.00268)
+    @test !isapprox(sar_structs[1].advantage_norm, -1.00268)
+
     empty!(sars)
-    @test map(x -> x.q, fill_q(sars)) == Float32.([])
+    @test map(x -> x.q, fill_q(sars, sar -> sar.q)) == Float32.([])
     @test length(sars.states) == 0
     @test length(sars.actions) == 0
     @test length(sars.rewards) == 0
