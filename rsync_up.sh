87094b552ffbac022473ec891e1c4f3b96271078
DevJac <dbuttonz@gmail.com>
2021-06-06 14:20:20 -0600

    Script to sync files to cloud

---
+++ rsync_up.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo 'Syncing solve_the_spire'
+rsync -ac --info=progress2 ~/Code/julia/solve_the_spire root@$DROPLET_IP:
+
+echo 'Syncing GOG Games'
+rsync -ac --info=progress2 ~/GOG\ Games root@$DROPLET_IP:
+
+echo 'Syncing ModTheSpire'
+rsync -ac --info=progress2 ~/.config/ModTheSpire root@$DROPLET_IP:
